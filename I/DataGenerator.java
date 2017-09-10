6addb6810f084cefc9a0f6c934c16f8b57c2e8d5
Minho Kim <minho1a@hanmail.net>
2017-09-10 20:27:05 +0900

    Fix generator for problem I.

--- I/DataGenerator.java
+++ I/DataGenerator.java
@@ -12,14 +12,25 @@ public class DataGenerator {
 		for (int i = 1; i <= 100; i += 2) {
 			N = i;
 			count++;
-			
+
 			int[][] input = new int[MAXCLUB][N];
 			generate(input);
-			print(input);
-			boolean OK = decision(solution(input));
+			HashMap<String, Integer> output = solution(input);
+			boolean OK = decision(output);
+
+			int max = 0;
+			String result = null;
+			for (String name : output.keySet()) {
+				if (output.get(name) > max) {
+					result = name;
+					max = output.get(name);
+				}
+			}
+
+			print(input, result);
 
 			if (OK)
-				save(input);
+				save(input, result);
 			else {
 				i -= 2;
 				count--;
@@ -30,10 +41,10 @@ public class DataGenerator {
 	private static void generate(int[][] input) {
 		for (int i = 0; i < MAXCLUB; i++)
 			for (int n = 0; n < N; n++)
-				input[i][n] = random(10, 50);
+				input[i][n] = random(1, 1000);
 	}
 
-	private static void print(int[][] input) {
+	private static void print(int[][] input, String result) {
 		System.out.println(N);
 		for (int i = 0; i < MAXCLUB; i++) {
 			System.out.print(input[i][0]);
@@ -41,42 +52,49 @@ public class DataGenerator {
 				System.out.print(" " + input[i][n]);
 			System.out.println();
 		}
+
+		System.out.println(result);
 	}
 
-	private static HashSet<String> solution(int[][] input) {
-		int[] maxAge = new int[MAXCLUB];
+	private static HashMap<String, Integer> solution(int[][] input) {
+		int[] maxScore = new int[MAXCLUB];
 		for (int i = 0; i < MAXCLUB; i++) {
 			int max = 0;
 			for (int n = 0; n < N; n++)
 				max = Math.max(max, input[i][n]);
-			maxAge[i] = max;
+			maxScore[i] = max;
 		}
 
-		int min = Integer.MAX_VALUE;
+		int max = 0;
 		for (int i = 0; i < MAXCLUB; i++)
-			min = Math.min(min, maxAge[i]);
+			max = Math.max(max, maxScore[i]);
 
-		HashSet<String> output = new HashSet<String>();
+		HashMap<String, Integer> output = new HashMap<String, Integer>();
 		for (int i = 0; i < MAXCLUB; i++)
-			if (maxAge[i] == min)
-				output.add(CLUB[i]);
-
-		for (String name : output)
-			System.out.print(name + " ");
+			output.put(CLUB[i], maxScore[i]);
 
 		return output;
 	}
 
-	private static boolean decision(HashSet<String> output) {
-		if (output.size() != 1)
-			return false;
-		if (!output.contains("ANT"))
-			return false;
+	private static boolean decision(HashMap<String, Integer> output) {
+		boolean check = true;
+		String[] keySet = new String[MAXCLUB];
+		int iter = 0;
+		for (String key : output.keySet())
+			keySet[iter++] = key;
 
+		for (int i = 0; i < MAXCLUB; i++)
+			for (int j = 0; j < MAXCLUB; j++)
+				if (i != j)
+					if (output.get(keySet[i]) == output.get(keySet[j]))
+						check = false;
+
+		if (!check)
+			return false;
 		return true;
 	}
 
-	private static void save(int[][] input) throws Exception {
+	private static void save(int[][] input, String name) throws Exception {
 		BufferedWriter in = new BufferedWriter(new FileWriter("/home/kmh/ANT/I/data/" + count + ".in"));
 		BufferedWriter out = new BufferedWriter(new FileWriter("/home/kmh/ANT/I/data/" + count + ".out"));
 
@@ -90,7 +108,7 @@ public class DataGenerator {
 				in.write("\n");
 		}
 
-		out.write("ANT");
+		out.write(name);
 
 		in.close();
 		out.close();
