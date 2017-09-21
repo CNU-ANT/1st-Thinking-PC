538f24c65c5e7b4a7ebbdae5c05503d0774b7c00
Minho Kim <minho1a@hanmail.net>
2017-09-21 15:30:53 +0900

    Add solution for D.

---
+++ D/Solution.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String... args) {
+		Scanner input = new Scanner(System.in);
+		boolean[][] graph = new boolean[input.nextInt()][input.nextInt()];
+
+		for (int i = 0; i < graph[0].length; i++) {
+			int h = input.nextInt();
+			for (int j = graph.length - 1; h > 0; j--, h--)
+				graph[j][i] = true;
+		}
+
+		int answer = 0;
+		for (int i = 0; i < graph.length; i++) {
+			for (int j = 0; j < graph[i].length; j++) {
+				if (graph[i][j])
+					continue;
+
+				boolean left = false;
+				boolean right = false;
+				int leftIndex = j;
+				int rightIndex = j;
+
+				while (--leftIndex >= 0)
+					if (graph[i][leftIndex]) {
+						left = true;
+						break;
+					}
+
+				while (++rightIndex < graph[i].length)
+					if (graph[i][rightIndex]) {
+						right = true;
+						break;
+					}
+
+				if (left && right) {
+					for (int index = leftIndex + 1; index <= rightIndex - 1; index++) {
+						graph[i][index] = true;
+						answer++;
+					}
+				}
+			}
+		}
+
+		System.out.print(answer);
+	}
+}
